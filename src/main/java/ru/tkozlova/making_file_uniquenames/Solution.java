2c38ce1babd3d687689988d4b1f68b7564363bf3
Tamara Kozlova <t.kozlova@trueengineering.ru>
2023-08-07 08:22:53 +0600

    Fixed solution for "Making file unique names" issue

--- src/main/java/ru/tkozlova/making_file_uniquenames/Solution.java
+++ src/main/java/ru/tkozlova/making_file_uniquenames/Solution.java
@@ -1,108 +1,42 @@
 package ru.tkozlova.making_file_uniquenames;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 class Solution {
+    Map<String, Integer> folderNames = new HashMap<>();
+
     public String[] getFolderNames(String[] names) {
-        Map<String, List<List<Integer>>> folderNames = new HashMap<>();
+        String[] result = new String[names.length];
 
-        List<String> result = new ArrayList<>();
+        for (int i = 0; i < names.length; i++) {
+            String name = names[i];
 
-        for (var name : names) {
+            if (folderNames.containsKey(name)) {
+                int lastVersion = folderNames.get(name);
+                var nextName = name;
 
-            var base = name;
-            List<Integer> version = List.of();
+                while (folderNames.containsKey(nextName)) {
+                    lastVersion++;
+                    var builder = new StringBuilder();
+                    builder.append(name);
+                    builder.append('(');
+                    builder.append(lastVersion);
+                    builder.append(')');
 
-            if (name.contains("(")) {
-                base = name.substring(0, name.indexOf("("));
-                version = Arrays.stream(
-                                name.substring(name.indexOf("("))
-                                        .replace("(", "")
-                                        .split("\\)"))
-                        .map(Integer::valueOf).collect(Collectors.toList());
-            }
-
-            if (folderNames.containsKey(base)) {
-                List<List<Integer>> existedVersions = folderNames.get(base);
-                if (existedVersions.contains(version)) {
-                    var nextVersion = getNextVersion(existedVersions, version);
-                    insertByInc(existedVersions, nextVersion);
-
-                    result.add(concatFolderName(base, nextVersion));
-                } else {
-                    insertByInc(existedVersions, version);
-                    result.add(name);
+                    nextName = builder.toString();
                 }
+
+                folderNames.put(name, lastVersion);
+                folderNames.put(nextName, 0);
+
+                result[i] = nextName;
             } else {
-                List<List<Integer>> firstVersion = new LinkedList<>();
-                firstVersion.add(version);
-                folderNames.put(base, firstVersion);
+                folderNames.put(name, 0);
 
-                result.add(name);
-            }
-
-
-        }
-
-        return result.toArray(new String[result.size()]);
-    }
-
-    private String concatFolderName(String base, List<Integer> version) {
-        StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append(base);
-        version.forEach(v -> {
-            stringBuilder.append("(");
-            stringBuilder.append(v);
-            stringBuilder.append(")");
-        });
-        return stringBuilder.toString();
-    }
-
-    private void insertByInc(List<List<Integer>> existedVersions, List<Integer> version) {
-        for (var sortedVersion : existedVersions) {
-            if (moreVersion(sortedVersion, version)) {
-                existedVersions.add(existedVersions.indexOf(sortedVersion), version);
-                return;
+                result[i] = name;
             }
         }
-
-        existedVersions.add(version);
-    }
-
-    private boolean moreVersion(List<Integer> version, List<Integer> newVersion) {
-
-        for (var i = 0; i < newVersion.size(); i++) {
-            if (i >= version.size()) {
-                return false;
-            }
-
-            if (version.get(i) < newVersion.get(i)) {
-                return false;
-            } else if (version.get(i) > newVersion.get(i) && version.size() == i + 1) {
-                return true;
-            }
-
-        }
-
-        return false;
-    }
-
-    private List<Integer> getNextVersion(List<List<Integer>> existedVersion, List<Integer> version) {
-
-        int tailVersion = 1;
-        var newVersion = new ArrayList<>(version);
-        newVersion.add(tailVersion);
-        while (existedVersion.contains(newVersion)) {
-            tailVersion++;
-            newVersion.set(version.size(), tailVersion);
-        }
-
-        return newVersion;
+        return result;
     }
 }
--- src/test/java/ru/tkozlova/making_file_uniquenames/SolutionTest.java
+++ src/test/java/ru/tkozlova/making_file_uniquenames/SolutionTest.java
@@ -1,5 +1,6 @@
 package ru.tkozlova.making_file_uniquenames;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class SolutionTest {
@@ -8,16 +9,14 @@ public class SolutionTest {
     void test() {
         Solution solution = new Solution();
 
-        //System.out.println(List.of(solution.getFolderNames(new String[]{"kaido", "kaido(1)", "kaido", "kaido(1)"})));
+        Assertions.assertArrayEquals(new String[]{"kaido", "kaido(1)", "kaido(2)", "kaido(1)(1)"},
+                solution.getFolderNames(new String[]{"kaido", "kaido(1)", "kaido", "kaido(1)"}));
 
-        //System.out.println(List.of(solution.getFolderNames(new String[]{"onepiece","onepiece(1)","onepiece(2)","onepiece(3)","onepiece"})));
+        Assertions.assertArrayEquals(new String[]{"onepiece", "onepiece(1)", "onepiece(2)", "onepiece(3)", "onepiece(4)"}
+                ,solution.getFolderNames(new String[]{"onepiece","onepiece(1)","onepiece(2)","onepiece(3)","onepiece"}));
 
-        solution.getFolderNames(new String[]{"j", "u(3)", "u(2)", "x", "t", "t(4)", "u(3)", "x", "y", "n(1)(2)", "w", "j", "l", "j", "f", "g(3)(3)", "d(3)", "l", "v", "e", "k", "f(1)", "f(2)", "v(4)", "k", "n", "b", "g(1)(4)", "h", "t", "d(3)", "m(1)", "p", "p", "k", "s(1)", "f(1)", "k", "o", "o", "z(2)(4)", "r", "c", "p(1)", "j", "c", "s", "f", "g(4)(3)", "u(2)(4)", "r(3)(3)", "v", "a(1)",
-                "h", "q(3)", "k(4)", "d(3)(2)", "y(1)(1)", "q(1)(2)", "t", "i", "t", "q", "z", "r(3)(3)", "x", "k", "n(2)(2)", "h", "t(2)", "q", "j", "u", "r(3)", "h", "u", "e", "y", "u", "t", "q", "u(3)", "b(3)", "k",
-                "m(1)", "a(1)(2)", "p", "i", "q", "e", "q", "k", "c", "h", "l", "p", "i", "b(3)", "q", "f",
-                "i", "c", "c(3)", "l(2)(2)", "g", "d(1)", "y(4)", "r", "r(4)(3)", "u", "s", "g", "l", "j(1)(4)",
-                "w(1)", "q", "s", "g", "v", "a", "z", "x", "i", "i", "j", "e", "g(2)(2)", "r(2)(2)", "g", "u", "e",
-                "r(4)(1)", "e", "u", "u(4)(4)", "e", "h", "p", "j", "b(1)(3)", "g", "v", "b", "j(4)(3)", "w", "w", "c"
-        });
+        Assertions.assertArrayEquals(new String[]{"wano", "wano(1)", "wano(2)", "wano(3)"},
+                solution.getFolderNames(new String[]{"wano", "wano", "wano", "wano"}));
+
     }
 }
